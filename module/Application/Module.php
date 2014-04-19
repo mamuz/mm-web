808a1f5ae625f3aa2124ccb703ff1759d448d17f
mamuz <muzzi_is@web.de>
2014-04-19 21:51:13 +0200

    prepared skeleton

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,22 +1,20 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application;
 
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature;
 use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
 
-class Module
+class Module implements
+    Feature\AutoloaderProviderInterface,
+    Feature\BootstrapListenerInterface,
+    Feature\ConfigProviderInterface
 {
-    public function onBootstrap(MvcEvent $e)
+    public function onBootstrap(EventInterface $e)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
+        /** @var \Zend\Mvc\MvcEvent $e */
+        $eventManager = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
     }
