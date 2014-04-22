668e856e36d210bc2dafb76b48ef5f2060846604
mamuz <muzzi_is@web.de>
2014-04-22 23:58:11 +0200

    added UTs

--- public/index.php
+++ public/index.php
@@ -1,31 +1,13 @@
 <?php
 
+define('__INDEX_INCLUDED', true);
+
 if (file_exists('environment.php')) {
     require 'environment.php';
 }
 
-// Define environment
-$env = getenv('APPLICATION_ENV') ? : 'production';
+chdir(getenv('APPLICATION_PATH') ? : dirname(__DIR__));
 
-/**
- * Display all errors when environment is development.
- * Define request consts for Zend developer tools.
- */
-if ($env === 'development') {
-    define('REQUEST_MEMORY_USAGE', memory_get_usage());
-    define('REQUEST_MICROTIME', microtime(true));
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-}
-
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
-
-// Setup autoloading
 require 'init_autoloader.php';
 
-// Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
