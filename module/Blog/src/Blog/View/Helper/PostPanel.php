74d0dbd098a6c99a358954f66ff7a1aa8457be78
mamuz <muzzi_is@web.de>
2014-05-05 23:28:36 +0200

    added blog module

--- module/Blog/src/Blog/View/Helper/PostPanel.php
+++ module/Blog/src/Blog/View/Helper/PostPanel.php
@@ -10,29 +10,35 @@ class PostPanel extends AbstractHelper
     /**
      * {@link render()}
      */
-    public function __invoke(PostEntity $entity)
+    public function __invoke(PostEntity $entity, $headerLink = true)
     {
-        return $this->render($entity);
+        return $this->render($entity, $headerLink);
     }
 
     /**
      * @param PostEntity $entity
+     * @param bool       $headerLink
      * @return string
      */
-    public function render(PostEntity $entity)
+    public function render(PostEntity $entity, $headerLink = true)
     {
-        $url = $this->getView()->url(
-            'blogActivePost',
-            array(
-                'title' => $entity->getTitle(),
-                'id'    => $this->getView()->hashId($entity->getId())
-            )
-        );
+        $header = $entity->getTitle();
+
+        if ($headerLink) {
+            $url = $this->getView()->url(
+                'blogActivePost',
+                array(
+                    'title' => $entity->getTitle(),
+                    'id'    => $this->getView()->hashId($entity->getId())
+                )
+            );
+            $header = '<a href="' . $url . '">' . $header . '</a>';
+        }
 
         $html = '<div class="page-header">' . PHP_EOL
-            . '<h3><a href="' . $url . '">' . $entity->getTitle() . '</a></h3>' . PHP_EOL
+            . '<h3>' . $header . '</h3>' . PHP_EOL
             . '</div>' . PHP_EOL
-            . '<p>' . $entity->getContent() . '</p>' . PHP_EOL;
+            . $this->getView()->markdown($entity->getContent());
 
         return $html;
     }
