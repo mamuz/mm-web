7d44fc714ea00a9283130f8b006554dabcec6776
mamuz <muzzi_is@web.de>
2014-05-08 23:13:44 +0200

    added blog module

---
+++ scripts/inspect.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p build/logs
+mkdir -p build/coverage
+
+./vendor/bin/pdepend --summary-xml=./build/logs/summary.xml \
+    --jdepend-chart=./build/logs/pdepend.svg --overview-pyramid=./build/logs/pyramid.svg ./module
+
+./vendor/bin/phpunit -c ./test --coverage-html ./build/coverage --coverage-clover ./build/logs/clover.xml
+
+./vendor/bin/phpmd ./module html codesize,controversial,design,unusedcode --exclude test > ./build/logs/phpmd.html
+
+./vendor/bin/phpcs --ignore=test --report=full --report-file=./build/logs/phpcs.log ./module
+
+./vendor/bin/phpcpd --exclude="test" ./module > ./build/logs/phpcpd.log
+
+./vendor/bin/phploc --exclude="test" ./module/ > ./build/logs/report.log
---
+++ scripts/mkdata.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p ./data/cache
+mkdir -p ./data/DoctrineORMModule
+mkdir -p ./data/logs
+
+sudo chmod -R 0775 ./data
