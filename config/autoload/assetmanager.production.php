125317e69a9957860ab912b564717d52f49d1677
mamuz <muzzi_is@web.de>
2014-05-10 20:56:43 +0200

    added blog module

---
+++ config/autoload/assetmanager.production.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'asset_manager' => array(
+        'caching' => array(
+            'default' => array(
+                'cache'   => 'FilePathCache',
+                'options' => array(
+                    'dir' => './data/cache/assets',
+                ),
+            ),
+        ),
+    ),
+);
