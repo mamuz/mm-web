f0b49fb4341297f3a97924a39695ec7e655d4db9
mamuz <muzzi_is@web.de>
2014-05-16 23:08:18 +0200

    added blog module

---
+++ module/Application/test/ApplicationTest/View/Helper/AutoVersionlTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ApplicationTest\View\Helper;
+
+use Application\View\Helper\AutoVersion;
+
+class AutoVersionTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var AutoVersion */
+    protected $fixture;
+
+    /** @var string */
+    protected $docRoot = __DIR__;
+
+    protected function setUp()
+    {
+        $this->fixture = new AutoVersion($this->docRoot);
+    }
+
+    public function testInvokable()
+    {
+        $file = basename(__FILE__);
+        $invoke = $this->fixture;
+        list($basename, $ext) = explode('.', $file);
+        $this->assertRegExp(
+            '%/' . $basename . '\.[0-9]{10}\.' . $ext . '%',
+            $invoke('/' . $file)
+        );
+    }
+
+    public function testInvokableWithoutTrailingSlash()
+    {
+        $file = basename(__FILE__);
+        $invoke = $this->fixture;
+        list($basename, $ext) = explode('.', $file);
+        $this->assertSame(
+            $file,
+            $invoke($file)
+        );
+    }
+
+    public function testInvokableFileNotExists()
+    {
+        $file = '/foo';
+        $invoke = $this->fixture;
+        $this->assertSame(
+            $file,
+            $invoke($file)
+        );
+    }
+}
