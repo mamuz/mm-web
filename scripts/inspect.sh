13b65ea0c206a251348d072d1eb532b113f1db53
mamuz <muzzi_is@web.de>
2014-05-18 16:12:02 +0200

    added blog module

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -3,24 +3,25 @@
 rm -rf ./build
 
 mkdir -p ./build/logs
+mkdir -p ./build/logs/xml
 mkdir -p ./build/coverage
 mkdir -p ./build/codebrowser
 mkdir -p ./build/api
 
-./vendor/bin/pdepend --summary-xml=./build/logs/summary.xml \
+./vendor/bin/pdepend --summary-xml=./build/logs/xml/summary.xml \
     --jdepend-chart=./build/logs/pdepend.svg --overview-pyramid=./build/logs/pyramid.svg ./module
 
-./vendor/bin/phpunit -c ./test --coverage-html ./build/coverage --coverage-clover ./build/logs/clover.xml
+./vendor/bin/phpunit -c ./test --coverage-html ./build/coverage --coverage-clover ./build/logs/xml/clover.xml
 
 ./vendor/bin/phpmd ./module html codesize,controversial,design,unusedcode --exclude test > ./build/logs/phpmd.html
-./vendor/bin/phpmd ./module xml codesize,controversial,design,unusedcode --exclude test > ./build/logs/phpmd.xml
+./vendor/bin/phpmd ./module xml codesize,controversial,design,unusedcode --exclude test > ./build/logs/xml/phpmd.xml
 
 ./vendor/bin/phpcs --standard=PSR2 --ignore=test,autoload_classmap.php,module.config.php --report-file=./build/logs/phpcs.log ./module
-./vendor/bin/phpcs --standard=PSR2 --ignore=test,autoload_classmap.php,module.config.php --report=xml --report-file=./build/logs/phpcs.xml ./module
+./vendor/bin/phpcs --standard=PSR2 --ignore=test,autoload_classmap.php,module.config.php --report=xml --report-file=./build/logs/xml/phpcs.xml ./module
 
 for D in `find ./module -maxdepth 1 -type d`
 do
-    ./vendor/bin/phpcpd --log-pmd=./build/logs/phpcpd_`basename ${D}`.xml --min-lines=3 --min-tokens=20 --exclude=test ${D} > ./build/logs/phpcpd_`basename ${D}`.log
+    ./vendor/bin/phpcpd --log-pmd=./build/logs/xml/phpcpd_`basename ${D}`.xml --min-lines=3 --min-tokens=20 --exclude=test ${D} > ./build/logs/phpcpd_`basename ${D}`.log
 done
 
 ./vendor/bin/phploc --exclude="test" ./module > ./build/logs/phploc.log
@@ -29,6 +30,6 @@ done
 
 ./vendor/bin/security-checker security:check ./composer.lock > ./build/logs/security.log
 
-./vendor/bin/phpcb --log ./build/logs -o ./build/codebrowser -i test -s ./module
+./vendor/bin/phpcb --log ./build/logs/xml -o ./build/codebrowser
 
 phpdoc -i *test*,*Test,autoload_classmap.php,module.config.php -d ./module -t ./build/api
