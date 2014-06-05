77382e90aaf7d6cdb8caf43b5db3d58edd04b09a
mamuz <muzzi_is@web.de>
2014-06-05 23:08:14 +0200

    build optimized

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,5 +1,17 @@
 <?php
 
+/* @todo
+require_once __DIR__ . '/vendor/zendframework/zendframework/library/Zend/Loader/AutoloaderFactory.php';
+require_once __DIR__ . '/vendor/zendframework/zendframework/library/Zend/Loader/ClassMapAutoloader.php';
+Zend\Loader\AutoloaderFactory::factory(
+array(
+'Zend\Loader\ClassMapAutoloader' => array(
+'Composer' => __DIR__ . '/vendor/composer/autoload_classmap.php',
+)
+)
+);
+ */
+
 // Composer autoloading
 if (file_exists('vendor/autoload.php')) {
     $loader = include 'vendor/autoload.php';
