ceeba1e19ab94991879cf7ad1c5ac7f4e087c1ad
mamuz <muzzi_is@web.de>
2015-02-24 00:03:38 +0100

    remove ocra service tool remove deprecated nav items

--- config/autoload/navigation.global.php
+++ config/autoload/navigation.global.php
@@ -23,127 +23,13 @@ return array(
             ),
         ),
         'default'       => array(
-            'home'         => array(
+            'home' => array(
                 'label' => 'Home',
                 'route' => 'home',
             ),
-            'blog'         => array(
+            'blog' => array(
                 'label' => 'Blog',
                 'uri'   => '/blog',
-            ),
-            /*
-            'frontend'     => array(
-                'label' => 'Frontend',
-                'uri'   => '#',
-                'pages' => array(
-                    'introduction'      => array(
-                        'label' => 'Introduction',
-                        'uri'   => '/frontend/introduction',
-                    ),
-                    'html'              => array(
-                        'label' => 'HTML',
-                        'uri'   => '/frontend/html',
-                    ),
-                    'css'               => array(
-                        'label' => 'CSS',
-                        'uri'   => '/frontend/css',
-                    ),
-                    'javascript'        => array(
-                        'label' => 'JavaScript',
-                        'uri'   => '/frontend/javascript',
-                    ),
-                    'user-experience'   => array(
-                        'label' => 'User Experience',
-                        'uri'   => '/frontend/user-experience',
-                    ),
-                    'responisve-design' => array(
-                        'label' => 'Responsive Design',
-                        'uri'   => '/frontend/responsive+design',
-                    ),
-                ),
-            ),
-            */
-            'oop'          => array(
-                'label' => 'OOP',
-                'uri'   => '#',
-                'pages' => array(
-                    'php'             => array(
-                        'label' => 'PHP',
-                        'uri'   => '/oop/php',
-                    ),
-                    'principles' => array(
-                        'label' => 'Principles',
-                        'uri'   => '/oop/principles',
-                    ),
-                    'clean-code'      => array(
-                        'label' => 'Clean Code',
-                        'uri'   => '/oop/clean+code',
-                    ),
-                    'solid'           => array(
-                        'label' => 'SOLID',
-                        'uri'   => '/oop/solid',
-                    ),
-                    'design-patterns' => array(
-                        'label' => 'Design Patterns',
-                        'uri'   => '/oop/design+patterns',
-                    ),
-                ),
-            ),
-            'architecture' => array(
-                'label' => 'Architecture',
-                'uri'   => '#',
-                'pages' => array(
-                    'planning'        => array(
-                        'label' => 'Planning',
-                        'uri'   => '/architecture/planning',
-                    ),
-                    'conventions'     => array(
-                        'label' => 'Conventions',
-                        'uri'   => '/architecture/conventions',
-                    ),
-                    'principles'      => array(
-                        'label' => 'Principles',
-                        'uri'   => '/architecture/principles',
-                    ),
-                    'layers'          => array(
-                        'label' => 'Layers / Tiers',
-                        'uri'   => '/oop/layers',
-                    ),
-                    'aspect-oriented' => array(
-                        'label' => 'Aspect-Oriented',
-                        'uri'   => '/oop/aspect-oriented',
-                    ),
-                    'documentation'   => array(
-                        'label' => 'Documentation',
-                        'uri'   => '/architecture/documentation',
-                    ),
-                ),
-            ),
-            'agile'        => array(
-                'label' => 'Agile',
-                'uri'   => '#',
-                'pages' => array(
-                    'scrum'                 => array(
-                        'label' => 'Scrum',
-                        'uri'   => '/agile/scrum',
-                    ),
-                    'continous-integration' => array(
-                        'label' => 'Continous Integration',
-                        'uri'   => '/agile/continous+integration',
-                    ),
-                    'domain-driven'         => array(
-                        'label' => 'Domain-Driven',
-                        'uri'   => '/agile/domain-driven',
-                    ),
-                    'test-driven'           => array(
-                        'label' => 'Test-Driven',
-                        'uri'   => '/agile/test-driven',
-                    ),
-                    'behavior-driven'       => array(
-                        'label' => 'Behavior-Driven',
-                        'uri'   => '/agile/behavior-driven',
-                    ),
-                ),
             ),
         ),
     ),
--- environment.php
+++ environment.php
@@ -1,3 +1,5 @@
 <?php
 
 putenv("APPLICATION_ENV=development");
+ini_set('display_errors', true);
+error_reporting(E_ALL);
