496fd0997e2917add900081c90b3aa8669ed2d15
mamuz <muzzi_is@web.de>
2015-02-24 23:27:29 +0100

    overwrite contact template

---
+++ config/autoload/contact.global.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'view_manager' => array(
+        'template_map' => array(
+            // @codingStandardsIgnoreStart
+            'mamuz-contact/command/create' => './module/Application/view/mamuz-contact/command/create.phtml',
+            // @codingStandardsIgnoreEnd
+        )
+    ),
+);
