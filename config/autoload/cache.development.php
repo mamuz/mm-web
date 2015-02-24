17f7dc8e5c350a24cb17817015077a018e37900e
mamuz <muzzi_is@web.de>
2015-02-24 15:19:46 +0100

    adapt configs to env

---
+++ config/autoload/cache.development.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'caches' => array(
+        'outputCache' => array(
+            'options' => array(
+                'readable' => false,
+                'writable' => false,
+            ),
+        ),
+    ),
+);
