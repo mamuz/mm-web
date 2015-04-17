53a8ee3ea0efba19f37cd0027ac2a2e237f9c410
mamuz <muzzi_is@web.de>
2015-04-18 00:09:48 +0200

    simplify inspection script

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -5,8 +5,9 @@ rm -rf ./build
 mkdir -p ./build/logs
 mkdir -p ./build/coverage
 
-./vendor/bin/pdepend --jdepend-chart=./build/logs/pdepend.svg --overview-pyramid=./build/logs/pyramid.svg ./module
-./vendor/bin/phpunit -c ./tests --coverage-html ./build/coverage
-./vendor/bin/phpmd ./module html codesize,controversial,design,unusedcode --exclude tests > ./build/logs/phpmd.html
-./vendor/bin/phpcs --standard=PSR2 --ignore=tests,autoload_classmap.php,plugin_classmap.php,template_map.php,module.config.php --report-file=./build/logs/phpcs.log ./module
+pdepend --jdepend-chart=./build/logs/pdepend.svg --overview-pyramid=./build/logs/pyramid.svg ./module
+phpunit -c ./tests --coverage-html ./build/coverage
+phpcpd ./module > ./build/logs/phpcpd.txt
+phpmd ./module html codesize,controversial,design,unusedcode --exclude tests > ./build/logs/phpmd.html
+phpcs --standard=PSR2 --ignore=tests,autoload_classmap.php,plugin_classmap.php,template_map.php,module.config.php --report-file=./build/logs/phpcs.log ./module
 ./vendor/bin/security-checker security:check ./composer.lock > ./build/logs/security.log
